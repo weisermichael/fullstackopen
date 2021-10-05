47b55ef0ca567ead4625e11939da8941e9431a6f
weisermichael <weiserjmichael@gmail.com>
2021-10-05 09:54:34 -0700

    phonebook thorugh 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,19 +6,35 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    if ((persons.map(p=>p.name)).indexOf(newName) === -1){
+      event.preventDefault()
+      const newPerson = {name: newName}
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+    }
+    else{
+      alert(`${newName} is already added to phonebook!`)
+    }
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person=> <p key={person.name}>{person.name}</p>)}
     </div>
   )
 }
