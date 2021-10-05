25adbc46d0ddf182fdb43d71778a5ba35fc7e8c8
weisermichael <weiserjmichael@gmail.com>
2021-10-05 13:21:54 -0700

    phonebook through 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const DisplayPerson = ( {toShow} ) => (
   <>
@@ -22,12 +23,11 @@ const Filter = ( {handleFilter} ) => (
 )
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then(response => setPersons(response.data))
+  }, [])
 
   const [ newName, setNewName ] = useState('')
   const [ newNum, setNewNum ] = useState('')
