955811d3b4add7ed81ab8cb471aa4ed1260e3acf
weisermichael <weiserjmichael@gmail.com>
2021-10-12 18:36:51 -0700

    part 3 through 3.8

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+morgan.token('post', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
 let persons = [
     { 
