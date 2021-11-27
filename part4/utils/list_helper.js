8ece667f0f586b89ea777f7420b9ca264f4fafb7
weisermichael <weiserjmichael@gmail.com>
2021-11-26 23:13:25 -0800

    thru part 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,24 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((prev, current) => {
+        return prev + current.likes
+}, 0)
+}
+
+const listWithOneBlog = [
+    {
+        "_id": "619eebfe67099cc1e05759eb",
+        "title": "Michael's blog",
+        "author": "Michael Weiser",
+        "url": "michael.com",
+        "likes": 100,
+        "__v": 0
+    }
+]
+console.log(totalLikes(listWithOneBlog))
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
