91b83e205abba99793ebb9bf5990435ddabcf9ff
weisermichael <weiserjmichael@gmail.com>
2021-11-26 21:24:10 -0800

    thru 4.3

--- part4/controllers/blogrouter.js
+++ part4/controllers/blogrouter.js
@@ -9,6 +9,19 @@ blogRouter.get('/', (request, response) => {
       })
   })
   
+  blogRouter.get('/:id', (request, response) => {
+    Blog
+      .findById(request.params.id)
+      .then(blogs => {
+        if (blogs) {
+          response.json(blogs)
+        }
+        else{
+          response.status(404).end()
+        }
+      })
+  })
+
 blogRouter.post('/', (request, response) => {
     const blog = new Blog(request.body)
     console.log(request.body)
